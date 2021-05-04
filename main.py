4c1cd35afd8b0c0a30b47584a8bb628243e86b4a
Daniel Krill <Daniel.Krill90@googlemail.com>
2021-05-04 17:42:29 +0200

    main.py: wegschreibfunktion implementiert, einlesefunktion in arbeit

--- main.py
+++ main.py
@@ -1,48 +1,55 @@
-# This is a sample Python script.
 
 # Press Umschalt+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Beispiele
 
+# print_hi('PyCharm')
+#                  0      1       2
+# begruessen = ['hello', 'hi', 'hallo']
+# begruessen.append('hey')    # hey wird an das Ende von begruessen angehangt
+# begruessen.insert(0, 'hey')    # hey wird an den start von begruessen angehängt
+# begruessen.sort()    # sortiert begruessen alphabetisch
+# print_list(begruessen)
+# input1 = input() # Eingabe mit input()
+# print(input1)    # Ausgabe mit print()
 
-# def print_hi(name):
-#     # Use a breakpoint in the code line below to debug your script.
-#     print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
-# def print_list(list):
-#      for begruessung in list:
-#          print(begruessung)
-
-# todo Groß- und Kleinschreibung sollte bei "eingabe" uninteressant sein
+import pickle
 from random import randrange
 
+# Variablen die in mehreren Bereichen genutzt werden
+begruessung = ['Hallo', 'Hey', 'Guten Tag', 'Hi', 'Moin']
+verabschiedung = ['Tschüss', 'Bye', 'Auf Wiedersehen']
 
-def antwort_finden(eingabe):
-    begruessung = ['Hallo', 'Hey', 'Guten Tag', 'Hi', 'Moin']
+
+def wegschreiben(liste, dateiname):
+    with open(dateiname, 'wb') as tmp:
+        pickle.dump(liste, tmp)
+
+
+def einlesen(dateiname, liste):
+    with open(dateiname, 'rb') as tmp:
+        liste = pickle.load(tmp)
+
+
+def antwort_finden(eingabe):    # todo Groß- und Kleinschreibung sollte bei "eingabe" uninteressant sein
+
     if eingabe in begruessung:
-        return begruessung[randrange(len(begruessung))]   # Zufällige Antwort
+        return begruessung[randrange(len(begruessung))]   # Zufällige Antwort aus bergruessung
     else:
-        return "Ich habe Sie nicht verstanden."    # todo Eweiterung mit Liste von Entschuldigungen
+        if eingabe in verabschiedung:
+            return verabschiedung[randrange(len(verabschiedung))]   # Zufällige Antwort aus verabschiedung
+        else:
+            return "Ich habe Sie nicht verstanden."    # todo Eweiterung mit Liste von Entschuldigungen
 
 
 def simple_chat_bot():
     eingabe = ""    # ist lokale Variable?
-    verabschiedung = ['Tschüss', 'Bye', 'Auf Wiedersehen']
     while eingabe not in verabschiedung:
         eingabe = input()
         antwort = antwort_finden(eingabe)
         print(antwort)
+    wegschreiben(verabschiedung, 'verabschiedung.txt')
 
 
 if __name__ == '__main__':   # Die Konstruktion if __name__ == ”__main__” wird eingesetzt um eine Python Datei als eigenständiges Programm zu nutzen und einzelne Elemente dieser Datei importierbar zu machen.
-
-    # Beispiele
-
-    # print_hi('PyCharm')
-    #                  0      1       2
-    # begruessen = ['hello', 'hi', 'hallo']
-    # begruessen.append('hey')    # hey wird an das Ende von begruessen angehangt
-    # begruessen.insert(0, 'hey')    # hey wird an den start von begruessen angehängt
-    # begruessen.sort()    # sortiert begruessen alphabetisch
-    # print_list(begruessen)
-    # input1 = input() # Eingabe mit input()
-    # print(input1)    # Ausgabe mit print()
     simple_chat_bot()
